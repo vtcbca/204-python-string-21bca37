9dec483ca1fcd09f2ed655fe6b7729aeda773e16
21bca37 <96609373+21bca37@users.noreply.github.com>
2022-03-24 11:21:29 +0530

    Add files via upload
    only list is printing  please solve error

---
+++ solution/listeven.py
@@ -0,0 +1,16 @@
+def createlist():
+    l=[]
+    for i in range(5):
+        s=input("Enter string:")
+        l.append(s)
+def strlen(l):
+    count=0
+    for i in l:
+        for j in i:
+            if(l%2==0):
+                count+=1
+    return(count)
+
+createlist()
+ans=strllen(l)
+print(ans)
